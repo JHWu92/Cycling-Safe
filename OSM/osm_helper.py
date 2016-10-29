c986375430cd184ddb3428c75a913a68eea748ac
JHWu92 <jhwu92@gmail.com>
2016-10-29 12:11:12 -0400

    update function in osm_helper.py

--- OSM/osm_helper.py
+++ OSM/osm_helper.py
@@ -11,26 +11,21 @@ def node2pt(node):
     return shpgeo.Point(node.lon,node.lat)
 
 
-def lonlat_in_way(osm_data, way):
+def way2line(osm_data, way):
     nodes = [osm_data.get_osm_node_by_id(nid) for nid in way.nodes]
-    return [(node.lon, node.lat) for node in nodes]
+    return shpgeo.LineString([(node.lon, node.lat) for node in nodes])
 
 
-def way2line(osm_data, wid):
-    way = osm_data.get_osm_way_by_id(wid)
-    return shpgeo.LineString(lonlat_in_way(osm_data, way))
-
-
-def rltn2poly(osm_data, rid):
+def rltn2poly(osm_data, relation):
     """
     work for only continuous lines. if the linestring is not closed, 
     a new line between the first and last node will be added
     """
     cltn = []
-    relation = osm_data.get_osm_relation_by_id(rid)
     for m in relation.members:
         if m.type==Way:
-            ln = way2line(osm_data, m.member_id)
+            way = osm_data.get_osm_way_by_id( m.member_id)
+            ln = way2line(osm_data, way)
             cltn.append(ln)
     merged_line = linemerge(cltn)
     return shpgeo.Polygon(merged_line)
